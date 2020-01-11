9a64910808e68447525c5c70653bf975ec20d2b6
dawid-t <dawiddt@wp.pl>
2020-01-11 01:14:08 +0100

    Add Player Movement and Input scripts

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class PlayerInput : MonoBehaviour
+{
+	[SerializeField]
+	private Platform usedPlatform;
+	private PlayerMovement playerMovement;
+
+
+	public enum Platform { PC, AndroidVR }
+
+
+	private void Awake()
+	{
+		playerMovement = GetComponent<PlayerMovement>();
+	}
+
+	private void Update()
+	{
+		GetInputsFromRightPlatform();
+	}
+
+	private void GetInputsFromRightPlatform()
+	{
+		if(usedPlatform == Platform.PC)
+		{
+			GetMouseInputs();
+			GetKeyboardInputs();
+		}
+		else
+		{
+			GetJoystickInputs();
+			GetSmartphoneControllerInputs();
+		}
+	}
+
+	#region PC inputs:
+	private void GetMouseInputs()
+	{
+
+	}
+
+	private void GetKeyboardInputs()
+	{
+		playerMovement.Jump = Input.GetButtonDown("Jump");
+		playerMovement.X = Input.GetAxis("Horizontal");
+		playerMovement.Z = Input.GetAxis("Vertical");
+	}
+	#endregion PC inputs.
+
+	#region Android VR inputs:
+	private void GetJoystickInputs()
+	{
+
+	}
+
+	private void GetSmartphoneControllerInputs()
+	{
+
+	}
+	#endregion Android VR inputs.
+}
