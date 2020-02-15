c62edd01d2a938ff31695a72973dbe85d4d877c2
dawid-t <dawiddt@wp.pl>
2020-02-15 19:40:51 +0100

    Add PointsSpawner script
    PointsSpawner script allows, in simple way, add enemy patrol points
    in a scene view (in edit mode).

---
+++ Assets/Editor/PointsSpawnerEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PointsSpawner))]
+public class PointsSpawnerEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		PointsSpawner enemiesPatrolPoints = (PointsSpawner)target;
+
+		// Button:
+		string buttonName = enemiesPatrolPoints.CreateNewPoints ? "Stop creating new points" : "Create new points";
+		GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+		if(enemiesPatrolPoints.CreateNewPoints)
+		{
+			buttonStyle.normal.textColor = Color.red;
+			buttonStyle.active.textColor = Color.red;
+			buttonStyle.fontStyle = FontStyle.Bold;
+		}
+
+		if(GUILayout.Button(buttonName, buttonStyle))
+		{
+			if(!enemiesPatrolPoints.CreateNewPoints)
+			{
+				enemiesPatrolPoints.StartCreatingNewPoints();
+			}
+			else
+			{
+				enemiesPatrolPoints.StopCreatingNewPoints();
+			}
+		}
+
+		// Help box:
+		if(enemiesPatrolPoints.CreateNewPoints)
+		{
+			EditorGUILayout.HelpBox("Click a left mouse button on the scene view to set a new point.", MessageType.Info);
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy/PointsSpawner.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PointsSpawner : MonoBehaviour
+{
+	private bool createNewPoints;
+	private GameObject pointToInstantiate;
+
+
+	public bool CreateNewPoints => createNewPoints;
+
+
+	private void OnEnable()
+	{
+		SceneView.duringSceneGui += CreatePoint;
+	}
+
+	private void OnDisable()
+	{
+		SceneView.duringSceneGui -= CreatePoint;
+	}
+
+	public void StartCreatingNewPoints()
+	{
+		createNewPoints = true;
+		OnCreatingNewPoints(createNewPoints);
+
+		CreateNewPointToInstantiate(transform.position);
+	}
+
+	public void StopCreatingNewPoints()
+	{
+		createNewPoints = false;
+		OnCreatingNewPoints(createNewPoints);
+
+		DestroyImmediate(pointToInstantiate);
+	}
+
+	private void OnCreatingNewPoints(bool isLocked)
+	{
+		ActiveEditorTracker.sharedTracker.isLocked = isLocked; // Lock/unlock the inspector.
+		Selection.activeGameObject = gameObject; // Select this gameObject.
+
+		string modeStatus = isLocked ? "ON" : "OFF";
+		Debug.Log("<color=red>Create enemies patrol points mode: <b>"+modeStatus+"</b></color>");
+	}
+
+	private void CreatePoint(SceneView scene)
+	{
+		if(!createNewPoints)
+		{
+			return;
+		}
+
+		// Get a current event:
+		Event e = Event.current;
+
+		// Show a point to instantiate:
+		if(e.type == EventType.MouseMove)
+		{
+			// Get a current mouse position on the scene view:
+			Vector3 mousePos = e.mousePosition;
+			float ppp = EditorGUIUtility.pixelsPerPoint;
+			mousePos.y = scene.camera.pixelHeight - mousePos.y * ppp;
+			mousePos.x *= ppp;
+
+			// Raycast on the scene view:
+			Ray ray = scene.camera.ScreenPointToRay(mousePos);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit))
+			{
+				pointToInstantiate.transform.position = hit.point;
+			}
+		}
+
+		// Create the new point to instantiate (the current point will stay):
+		if(e.type == EventType.MouseDown && e.button == 0)
+		{
+			CreateNewPointToInstantiate(pointToInstantiate.transform.position);
+			EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+			e.Use();
+		}
+	}
+
+	private void CreateNewPointToInstantiate(Vector3 firstPosition)
+	{
+		pointToInstantiate = new GameObject((transform.childCount+1)+"");
+		pointToInstantiate.transform.parent = transform;
+		pointToInstantiate.transform.position = firstPosition;
+		DrawPointIcon(pointToInstantiate);
+	}
+
+	private void DrawPointIcon(GameObject go)
+	{
+		GUIContent icon = EditorGUIUtility.IconContent("sv_label_6");
+		Type egu = typeof(EditorGUIUtility);
+
+		BindingFlags flags = BindingFlags.InvokeMethod | BindingFlags.Static | BindingFlags.NonPublic;
+		object[] args = new object[] { go, icon.image };
+
+		egu.InvokeMember("SetIconForObject", flags, null, null, args);
+	}
+}
